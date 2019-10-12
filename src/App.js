a3b4a1e90e73a8efabb2e4db15716ba9189ebd06
fanzhen <fanz@cpwxw.net>
2019-10-12 13:45:18 +0800

    commit-ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import Demo from './Demo';
+import Add from './component/add/Add'
+import List from './component/list/List'
 
 function App() {
   return (
     <div>
-      <Demo/>
-      <h2>这是一个简单的react项目</h2>
+       <header className="site-header jumbotron">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <h1>请发表对React的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+       <div className="container">
+        <Add/>
+        <List/>
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
